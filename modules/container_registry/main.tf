d97690ad491b512e106629e976623610eb7a27da
marclamy <marc.lamy83@gmail.com>
2023-12-24 05:49:45 +0100

    rename the acr

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "acr_rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "novacp${var.environment}${var.region_short}acr"
+  name                = "novasoftworks"
   resource_group_name = azurerm_resource_group.acr_rg.name
   location            = azurerm_resource_group.acr_rg.location
   sku                 = "Basic"
