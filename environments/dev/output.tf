bf8f9959c8ed1cb6b2e88583a8fb577646402af5
marclamy <marc.lamy83@gmail.com>
2024-01-01 14:51:49 +0100

    add an output for the app service plan

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -3,3 +3,8 @@ output "acr" {
   description = "The Azure Container Registry."
   sensitive   = true
 }
+
+output "app_service_plan" {
+  value       = module.app_service_plan.app_service_plan
+  description = "The App Service Plan."
+}
---
+++ modules/app_service_plan/output.tf
@@ -0,0 +1,4 @@
+output "app_service_plan" {
+  value       = azurerm_service_plan.app_service_plan
+  description = "The App Service Plan."
+}
