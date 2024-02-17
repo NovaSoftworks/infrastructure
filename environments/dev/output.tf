aaa19cec4fe350d27b01b159a6be0b9b6234e26b
marclamy <marc.lamy83@gmail.com>
2024-02-17 10:29:36 +0100

    refactor outputs of PaaS k8s

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,10 +1,27 @@
+locals {
+  kube_config = module.paas.paas_k8s.kube_config.0
+}
+
 output "dns_zone_novasoftworks_com" {
   value       = module.dns.dns_zone_novasoftworks_com
   description = "The DNS zone for the domain novasoftworks.com."
 }
 
-output "k8s" {
-  value       = module.k8s.k8s
-  description = "The Kubernetes cluster."
-  sensitive   = true
+output "paas_k8s_host" {
+  value = local.kube_config.host
+}
+
+output "paas_k8s_client_certificate" {
+  value     = base64decode(local.kube_config.client_certificate)
+  sensitive = true
+}
+
+output "paas_k8s_client_key" {
+  value     = base64decode(local.kube_config.client_key)
+  sensitive = true
+}
+
+output "paas_k8s_cluster_ca_certificate" {
+  value     = base64decode(local.kube_config.cluster_ca_certificate)
+  sensitive = true
 }
